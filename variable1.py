d04286d515105e8faecf0b87747049f45399ce86
SerJv21 <serjv@live.ru>
2024-08-27 21:31:39 +0300

    Initial commit

---
+++ variable1.py
@@ -0,0 +1,6 @@
+kolichestvo_vypolnenih_dc = 12
+kolichestvo_zatrachenih_chasov = 1.5
+nazvanie_kursa = 'Python'
+vremya_na_odno_zadanie = 0.125
+print(kolichestvo_zatrachenih_chasov / kolichestvo_vypolnenih_dc)
+print('Курс:'+nazvanie_kursa+',',"Всего задач:" + str(kolichestvo_vypolnenih_dc) +',', "затраченно часов:" + str(kolichestvo_zatrachenih_chasov)+',', "среднее время выполнения:" + str(vremya_na_odno_zadanie)+'.')
